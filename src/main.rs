3ec2332a50f47187cbdc20001ce50b7533203c54
pseusys <aleksandr.sergeev.ad@gmail.com>
2025-06-26 01:35:15 +0200

    resolve

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,22 @@ use std::net::{IpAddr, Ipv4Addr};
 use env_logger::init;
 use log::{debug, info};
 use socket2::{Domain, Protocol, Socket, Type};
-use tokio::net::TcpSocket;
+use tokio::net::{lookup_host, TcpSocket};
 use windivert::{CloseAction, WinDivert};
 use windivert::prelude::WinDivertFlags;
 
 type DynResult<T> = Result<T, Box<dyn Error + Sync + Send>>;
 
 
-async fn test_internet_access(number: usize, address: Ipv4Addr, port: u16, local: Option<Ipv4Addr>) -> DynResult<()> {
+async fn test_internet_access(number: usize, address: IpAddr, port: u16, local: Option<Ipv4Addr>) -> DynResult<()> {
     debug!("TEST BLOCK {number} STARTED");
-    let peer_address = SocketAddr::new(IpAddr::V4(address), port);
+    let peer_address = SocketAddr::new(address, port);
 
     let socket = Socket::new(Domain::IPV4, Type::STREAM, Some(Protocol::TCP))?.into();
     let connection_socket = TcpSocket::from_std_stream(socket);
 
     if let Some(addr) = local {
-        let local_address = SocketAddr::new(IpAddr::V4(addr), 0);
+        let local_address = SocketAddr::new(addr, 0);
         debug!("Binding connection client to {}...", local_address);
         connection_socket.bind(local_address)?;
     }
@@ -37,8 +37,9 @@ async fn test_internet_access(number: usize, address: Ipv4Addr, port: u16, local
 #[tokio::main]
 async fn main() -> DynResult<()> {
     init();
-    let test_ip = Ipv4Addr::new(8, 8, 4, 4);
     let test_port = 80;
+    let test_host = format!("example.com:{port}");
+    let test_ip = lookup_host(test_host).await?.next().map(|a| a.ip()).ok_or_else(|| Error::new("No IP found"))?;
 
     info!("Testing internet connection without WinDivert...");
     test_internet_access(1, test_ip, test_port, None).await?;
